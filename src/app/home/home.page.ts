ad82dfc8f8244c09288ba3bc0675f47782cd2c4f
Uma_Maheswari_K <umakrish0810@gmail.com>
2024-10-15 11:22:29 +0530

    updated

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,72 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { IonHeader, IonRefresherContent,IonToolbar, IonTitle, IonContent,IonItem,IonButton,IonActionSheet,IonCard,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent} from '@ionic/angular/standalone';
+import { ActionSheetController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader,CommonModule,IonContent, IonToolbar, IonTitle, IonItem,IonContent,IonButton,IonActionSheet,IonCard,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent],
 })
 export class HomePage {
+  // public isCardVisible: boolean = true; // State to control card visibility
+
+  // public actionSheetButtons = [
+  //   {
+  //     text: 'Delete',
+  //     role: 'destructive',
+  //     data: {
+  //       action: 'delete',
+  //     },
+     
+  //   },
+  //   {
+  //     text: 'Share',
+  //     data: {
+  //       action: 'share',
+  //     },
+  //     handler: () => {
+  //       console.log('Share action'); // Handle share action here if needed
+  //     },
+  //   },
+  //   {
+  //     text: 'Cancel',
+  //     role: 'cancel',
+  //     data: {
+  //       action: 'cancel',
+  //     },
+  //   },
+  // ];
+
   constructor() {}
+
+  isActionSheetOpen = false;
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Share',
+      data: {
+        action: 'share',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
+
+  setOpen(isOpen: boolean) {
+    this.isActionSheetOpen = isOpen;
+  }
 }
+
